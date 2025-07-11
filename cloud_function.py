56e62957286247b3e5aa977dbc748c837ba7a208
indrajithprabaswara <indrajithprabaswara@gmail.com>
2025-07-11 22:58:41 +0530

    chore: replace v1 codebase with version 2

--- cloud_function.py
+++
@@ -1,7 +0,0 @@
-from pollution_data_visualizer.app import collect_all_data, TASKS_COLLECT_CALLED
-
-def run(event, context):
-    """Entry point for Pub/Sub triggered Cloud Function."""
-    TASKS_COLLECT_CALLED.inc()
-    collect_all_data(force=True)
-    return 'ok'
--- pollution_data_visualizer/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from .app import app, socketio
-
-application = app
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    socketio.run(app, host='0.0.0.0', port=port)
